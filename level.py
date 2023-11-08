0a0408089d4a661ba8fc73659c89a4ddd10fecd3
mato215 <matej.krajnak@gmail.com>
2023-11-08 18:51:24 +0100

    player asety + tile assety

--- level.py
+++ level.py
@@ -1,4 +1,8 @@
 import pygame
+from settings import*
+from tile import Tile
+from player import Player
+
 class Level:
     def __init__(self):
         
@@ -9,6 +13,20 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
         
+        #sprite setup
+        self.create_map()
+    
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+           for col_index, col in enumerate(row):
+               x = col_index * TILESIZE
+               y = row_index * TILESIZE
+               #if col == 'x':
+                   #Tile((x, y), [self.visible_sprites])
+               if col == 'p':
+                   Player((x, y), [self.visible_sprites])
+
+                     
     def run(self):
         #update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        self.image = pygame.image.load(r'C:\Users\matej\OneDrive\Počítač\pigame\Schoolproject\rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
