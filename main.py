ff5ed8c3d6f3d4765493d81d00bb66c712ccac10
mbrodnan <115623372+mbrodnan@users.noreply.github.com>
2023-11-08 16:49:34 +0100

    Test

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from settings import *
+
+class Game:
+
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('IDK')
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
